e976d0d05bb9d32475edc07806459aab97a3c52c
Laxman Roy <88562390+laxmanroy52@users.noreply.github.com>
2023-03-27 01:07:11 +0600

    Add all data

--- main.js
+++ main.js
@@ -5,8 +5,55 @@ let submit = document.getElementById('submit');
 let response = document.getElementsByClassName('response')[0];
 
 //import all data
-//import {data} from '/data.js';
-alert("Hello");
+
+
+
+
+
+let data = {
+  // Phrases to start talking
+  hi: " Hello there!",
+  hello: " Hi.",
+  'whats up': "I am very well.",
+  'how are you': "I am very well. Thanks, for asking",
+  
+  
+  // Phrase to personal information
+  'who are you': "I am Laxman.",
+  'whats your name': "My name is Laxman roy.",
+  'what is your name': "My name is Laxman roy.",
+  'where are you from': "I am from Chirirbandar, Dinajpur, Bangladesh",
+  'your father name': "My father name is Parimol roy",
+  'who is your father': "My father is Parimol roy",
+  'your mother name': "My mother name is Champa rani",
+  'who is your mother': "My mother is Champa rani.",
+  'your brother name': "I have two brothers, including me we are three brothers. My elder brother name is Palash Roy and younger brother name is Molin Roy.",
+  'your elder brother name': "My elder brother's name is Polash Roy",
+  'who is your elder brother': "My elder brother's is Polash Roy",
+  'your younger brother name': "My younger brother name is Molin roy",
+  'who is your younger brother': "My younger brother is Molin roy",
+  'are you married': "No, I am not married yet",
+  'do you love someone': "Yes, I love my family.",
+  'do you love any girl': "No, I don't love any girl's.",
+  'do you have a girlfriend': "No, because I don't like these",
+  'how old are you': "My old is: 19",
+  'your height': "My height is 5.7 foot.",
+  'your weight': "My weight is: 55kg.",
+  
+  
+  
+  // Phrase to talking chatbot power
+  'can you help me': "Of course, I'd be happy to help! What can I assist you with? Please feel free to ask me any question or let me know what you need help with, and I'll do my best to provide you with the information or guidance you're looking for.",
+  
+ 'are you help me': "Yes, I am here to help you! Please let me know what you need assistance with, and I will do my best to provide you with the information or guidance you're looking for. Whether you have a question or need advice on a particular topic, I am here to support you to the best of my abilities.",
+}
+
+export {data};
+
+
+
+
+
 let send = () => {
   if (msg.value != '') {
   // get question
